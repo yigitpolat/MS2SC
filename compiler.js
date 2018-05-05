aca5df58c5c0f9890499c070b8e9890169f5d8b2
yp <yigit.polat@ozu.edu.tr>
2018-05-05 17:24:47 +0300

    fillListOfStrings printListOfStrings v2.4

--- compiler.js
+++ compiler.js
@@ -98,7 +98,7 @@ function pop(destination) {
 }
 
 function incrementSP(number) {
-    emit("ADDi", getMemoryAdress("stackPointer"), number.toString(), "");
+    emit("ADDi", getMemoryAdress("stackPointer"), number + "", "");
 }
 
 function push(source) {
@@ -131,26 +131,12 @@ var listOfCodes = [{type: "inst", location: 0, opCode: "BZJi", opA: "3", opB: "1
     {type: "data", location: 14, value: 0, comment: "//scratchMem4"},
     {type: "data", location: 15, value: 0, comment: "//scratchMem5"},
     {type: "data", location: 16, value: 0, comment: "//scratchMem6"},
-    {
-        type: "inst",
-        location: 17,
-        opCode: "CPi",
-        opA: "11",
-        opB: "24",
-        comment: "// $globalinit:  //17 \n// Calling main, numArgs: 0"
-    },
+    {type: "inst", location: 17, opCode: "CPi", opA: "11", comment: "// $globalinit:  //17 \n// Calling main, numArgs: 0"},
     {type: "inst", location: 18, opCode: "CPIi", opA: "1", opB: "11", comment: "// Push scratchMem1"},
     {type: "inst", location: 19, opCode: "ADDi", opA: "1", opB: "1", comment: ""},
     {type: "inst", location: 20, opCode: "CPIi", opA: "1", opB: "2", comment: "// Push basePointer"},
     {type: "inst", location: 21, opCode: "ADDi", opA: "1", opB: "1", comment: ""},
-    {
-        type: "inst",
-        location: 22,
-        opCode: "CP",
-        opA: "2",
-        opB: "1",
-        comment: "// Evaluating args.\n// Args evaluated.\n// Adjust BP to (SP - 0)"
-    },
+    {type: "inst", location: 22, opCode: "CP", opA: "2", opB: "1", comment: "// Evaluating args.\n// Args evaluated.\n// Adjust BP to (SP - 0)"},
     {type: "inst", location: 23, opCode: "BZJi", opA: "3", opB: "27", comment: ""},
     {type: "inst", location: 24, opCode: "ADD", opA: "1", opB: "4", comment: "// $L2:  //24\n// Pop to scratchMem1"},
     {type: "inst", location: 25, opCode: "CPI", opA: "11", opB: "1", comment: ""},
@@ -159,7 +145,7 @@ var listOfCodes = [{type: "inst", location: 0, opCode: "BZJi", opA: "3", opB: "1
 
 function modifyTopOfStack() {
     listOfCodes[1].value = listOfCodes[listOfCodes.length - 1].location + 1;
-    listOfCodes[2].value = listOfCodes[listOfCodes.length - 1].location + 1;
+    listOfCodes[2].value = listOfCodes[listOfCodes.length - 1].location + 1 ;
 }
 
 // function decreaseSP(){
@@ -184,11 +170,42 @@ function printListOfCodes() {
             console.log(listOfCodes[i].location + ": " + listOfCodes[i].opCode + " " + listOfCodes[i].opA + " " + listOfCodes[i].opB);
         } else if (listOfCodes[i].type === "data") {
             console.log(listOfCodes[i].location + ": " + listOfCodes[i].value + " " + listOfCodes[i].comment);
+        } else{
+            console.log(listOfCodes[i].comment);
         }
     }
 }
 
 function addVarDeclarationToListOfCodes(variable) {
+    var hashTable = hashList.getHead();
+    var comment = "";
+    if(doesValueExist(variable)) {
+        comment = "Initialization of var '" + variable.name + "'";
+        listOfCodes.push({comment : comment});
+        declarationOrStatement(variable.value);
+
+    }else {
+        comment = "Allocate var '" + variable.name + "'";
+        listOfCodes.push({comment : comment});
+        incrementSP(1);
+
+    }
+
+
+}
+
+function getNextLocation(){
+    let loc;
+    for(let i = listOfCodes.length -1; i>0; i--){
+        if(typeof listOfCodes[i].location !== 'undefined'){
+            loc = listOfCodes[i].location + 1;
+            break;
+        }
+    }
+    return loc;
+}
+
+    /*
     let comment = "";
     let name = variable.name;
     if (listOfCodes.length === 27) {
@@ -198,74 +215,30 @@ function addVarDeclarationToListOfCodes(variable) {
         if (typeof variable.value.value !== 'undefined') {
             let value = variable.value.value;
             comment += "// Initialization of var '" + name + "'\n// Const. int " + value;
-            listOfCodes.push({
-                type: "inst",
-                location: listOfCodes.length,
-                opCode: "CPi",
-                opA: "11",
-                opB: value,
-                comment: comment
-            });
-            listOfCodes.push({
-                type: "inst",
-                location: listOfCodes.length,
-                opCode: "CPIi",
-                opA: listOfCodes[1].location,
-                opB: "11",
-                comment: "// Push scratchMem1"
-            });
-            listOfCodes.push({
-                type: "inst",
-                location: listOfCodes.length,
-                opCode: "ADDi",
-                opA: listOfCodes[1].location,
-                opB: "1",
-                comment: ""
-            });
+            listOfCodes.push({type: "inst", location: listOfCodes.length, opCode: "CPi", opA: "11", opB: value, comment: comment});
+            listOfCodes.push({type: "inst", location: listOfCodes.length, opCode: "CPIi", opA: listOfCodes[1].location, opB: "11", comment: "// Push scratchMem1"});
+            listOfCodes.push({type: "inst", location: listOfCodes.length, opCode: "ADDi", opA: listOfCodes[1].location, opB: "1", comment: ""});
             listOfCodes[1].value += 1;
         } else {
             decideExpression(variable.value);
         }
     } else {
         comment += "// Allocate var '" + name + "'";
-        listOfCodes.push({
-            type: "inst",
-            location: listOfCodes.length,
-            opCode: "ADDi",
-            opA: "1",
-            opB: "1",
-            comment: comment
-        });
+        listOfCodes.push({type: "inst", location: listOfCodes.length, opCode: "ADDi", opA: "1", opB: "1", comment: comment});
     }
+    */
 
-}
+
 
 
 //----------------------- MAIN ---------------------------------
 for (let i = 0; i < myJSon.length; i++) {
+    listOfCodes.push({comment: "// Entering a block."});
     decideDeclaration(myJSon[i]);
+    listOfCodes.push({comment: "// Decrease SP by " + hashList.getHead().length})
+    decrementSP(hashList.getHead().length); //TODO
+
 }
-//
-// function assignOpCode(instruction) {
-//     switch (instruction) {
-//         case ("ADD"):
-//             return 0;
-//         case ("NAND"):
-//             return 1;
-//         case ("SRL"):
-//             return 2;
-//         case ("LT"):
-//             return 3;
-//         case ("CP"):
-//             return 4;
-//         case ("CPI"):
-//             return 5;
-//         case ("BZJ"):
-//             return 6;
-//         case ("MUL"):
-//             return 7;
-//     }
-// }
 
 function decideDeclaration(JSonObject) {
     var declaration = JSonObject.type;
@@ -362,8 +335,10 @@ function decideExpression(expression) {
     var expressionType = expression.type;
     switch (expressionType) {
         case ("Literal"):
-            var value = expression.value;
-            //addToEnvironment(key);
+            value = expression.value;
+            comment = "Const. int " + value + "";
+            emit("CPi", getMemoryAdress("scratchMem1"), "" + value + "", comment);
+            push("scratchMem1");
             return value;
         case ("Identifier"):
             var value = expression.value;
@@ -398,6 +373,10 @@ function doesElseExist(JSonBody) {
     return typeof JSonBody.else !== 'undefined';
 }
 
+function doesValueExist(JSonBody) {
+    return typeof JSonBody.value !== 'undefined';
+}
+
 
 function doBinaryExpression(expression) {
     var operator = expression.operator;
@@ -414,30 +393,9 @@ function doBinaryExpression(expression) {
             var opB = listOfCodes[1].value + lookup(leftValue);
             console.log(opB)
             var comment = "// Assignment\n// Const. int" + rightValue;  //??
-            listOfCodes.push({
-                type: "inst",
-                location: listOfCodes.length,
-                opCode: "CPi",
-                opA: "11",
-                opB: rightValue,
-                comment: comment
-            });
-            listOfCodes.push({
-                type: "inst",
-                location: listOfCodes.length,
-                opCode: "CPIi",
-                opA: "1",
-                opB: "11",
-                comment: "// Push scratchMem1"
-            });
-            listOfCodes.push({
-                type: "inst",
-                location: listOfCodes.length,
-                opCode: "ADDi",
-                opA: "1",
-                opB: "1",
-                comment: ""
-            });
+            listOfCodes.push({type: "inst", location: listOfCodes.length, opCode: "CPi", opA: "11", opB: rightValue, comment: comment});
+            listOfCodes.push({type: "inst", location: listOfCodes.length, opCode: "CPIi", opA: "1", opB: "11", comment: "// Push scratchMem1"});
+            listOfCodes.push({type: "inst", location: listOfCodes.length, opCode: "ADDi", opA: "1", opB: "1", comment: ""});
             return;
         case("+"):
             emit("ADD", getMemoryAdress("scratchMem1"), getMemoryAdress("scratchMem2"));
@@ -472,14 +430,7 @@ function doBinaryExpression(expression) {
 
 
 function emit(opCode, opA, opB, comment) {
-    listOfCodes.push({
-        type: "inst",
-        location: listOfCodes.length,
-        opCode: opCode,
-        opA: opA,
-        opB: opB,
-        comment: comment
-    });
+    listOfCodes.push({type: "inst", location: getNextLocation(), opCode: opCode, opA: opA, opB: opB, comment: comment});
 }
 
 function doSuffixExpression(operator, value) {
@@ -531,7 +482,7 @@ function removeFromEnvironment(key) {
 }
 
 
-decreaseSP();
+//decreaseSP();
 printListOfCodes();
 modifyTopOfStack();
 printTopOfStack();
